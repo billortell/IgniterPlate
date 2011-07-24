116dfbb21118cc3b75822750bb095c407bcb61c4
Wayn Hall <rwhall3@gmail.com>
2011-07-24 14:04:50 -0700

    Updated Build tool to work with css media queries. Added responsive design for mobile devices.

--- IgniterPlate/js/script.js
+++ IgniterPlate/js/script.js
@@ -1,3 +1,9 @@
 $(function(){
-
+    
+    // Hide URL Bar on iPhone and iPod Touch
+    if(navigator.userAgent.indexOf('iPhone') != -1) {
+        setTimeout(function () {
+            if (!pageYOffset) {window.scrollTo(0, 1);}
+        }, 1000);
+    }
 });
