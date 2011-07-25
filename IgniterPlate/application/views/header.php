0cf523d9628a3ba52fb4a0ec4720d982c415149c
Wayn Hall <rwhall3@gmail.com>
2011-07-25 07:01:34 -0700

    Commented out trailing slash Options 1 and 2

--- IgniterPlate/application/views/header.php
+++ IgniterPlate/application/views/header.php
@@ -1,24 +1,3 @@
 <header class="clearfix">
-    
-    <div class="left two-thirds">
-    
-        <h1>IgniterPlate</h1>
-        
-        <p><strong>Uniting the power of <?php echo anchor('http://codeigniter.com', 'CodeIgniter', 'target="_blank"'); ?>
-            and <?php echo anchor('http://html5boilerplate.com', 'HTML5 Boilerplate', 'target="_blank"'); ?></strong></p>
-        
-        <p>so you can code the faster, more powerful websites... Now!</p>
-        
-        <img id="logo" src="/img/igniterplate.png" alt="IgniterPlate">
-            
-    </div>
-    
-       
-    <?php echo anchor('user_guide', 'CodeIgniter User Guide', 'class="button" target="_blank"');  ?>
- 
-    <div class="right third">
-        
-        <?php // echo anchor('https://github.com/waynhall/IgniterPlate', 'Download from GitHub', 'class="big button" target="_blank"');  ?>
-        
-    </div>
+
 </header>
--- IgniterPlate/application/views/home.php
+++ IgniterPlate/application/views/home.php
@@ -1,30 +1,4 @@
 <article>
     
-    <div class="left quarter">
-        <h2>Fast</h2>
-        <p>Minify your css and javaScript, compress images, and cache resources
-        using HTML5 Boilerplate's build tool,
-        while utilizing CodeIgniter's exceptional performance, to greatly improve
-        site loading speeds.</p>
-    </div>
-    
-    <div class="right quarter">
-        <h2>Powerful</h2>
-        <p>Quickly create powerful HTML5 websites and apps! The Boilerplate gives
-        you a rock-solid starting point. CodeIgniter makes development easy with its array of helpers,
-        libraries, and clear documentation.</p>
-    </div>
-    
-    <div class="right quarter">
-        <h2>Clean</h2>
-        <p>Keep your code clean with CodeIgniter's MVC (Model-View-Controller) Structure,
-        Object-Oriented Design, and DRY (Don't Repeat Yourself) Principles.</p>
-    </div>
-    
-    <div class="right quarter">
-        <h2>Light</h2>
-        <p>Become a minimalist and code your own functionality. Stop relying on a CMS that's bloated with countless rigid
-        plugins and extensions.</p>
-    </div>
  
 </article>
