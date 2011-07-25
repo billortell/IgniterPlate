62b9a2d0a1cc7adb442354d051c786974794131d
Wayn Hall <rwhall3@gmail.com>
2011-07-25 02:36:40 -0700

    Added root paths (/) instead of relative paths in template.php and build.xml

--- IgniterPlate/application/views/template.php
+++ IgniterPlate/application/views/template.php
@@ -16,15 +16,15 @@
 	
 	<link rel="shortcut icon" href="/favicon.ico">
 	<link rel="apple-touch-icon" href="/apple-touch-icon.png">
-	<link rel="stylesheet" href="css/style.css?v=2">
+	<link rel="stylesheet" href="/css/style.css?v=2">
 
-	<script src="js/libs/modernizr-1.7.min.js"></script>
+	<script src="/js/libs/modernizr-1.7.min.js"></script>
 </head>
 <body>
 	<div id="container">
 		
-		<?php $this->load->view('header'); ?>
-
+	<?php $this->load->view('header'); ?>
+	
 		<div id="main" role="main">
 			
                     <?php $this->load->view($main_content); ?>
@@ -38,19 +38,19 @@
 	</div>
 
 	<script src="//ajax.googleapis.com/ajax/libs/jquery/1.6.2/jquery.min.js"></script>
-	<script>!window.jQuery && document.write(unescape('%3Cscript src="js/libs/jquery-1.6.2.min.js"%3E%3C/script%3E'))</script>
+	<script>!window.jQuery && document.write(unescape('%3Cscript src="/js/libs/jquery-1.6.2.min.js"%3E%3C/script%3E'))</script>
 
 	<!-- scripts concatenated and minified via ant build script-->
-	<script src="js/plugins.js"></script>
-	<script src="js/script.js"></script>
+	<script src="/js/plugins.js"></script>
+	<script src="/js/script.js"></script>
 	<!-- end scripts-->
   
 	<!--[if lt IE 7 ]>
-	<script src="js/libs/dd_belatedpng.js"></script>
+	<script src="/js/libs/dd_belatedpng.js"></script>
 	<script> DD_belatedPNG.fix('img, .png_bg');</script>
 	<![endif]-->
 	<script>
-		var _gaq=[['_setAccount','UA-XXXXX-X'],['_trackPageview']]; // Change UA-XXXXX-X to be your site's ID
+		var _gaq=[['_setAccount','UA-XXXXXXXX-X'],['_trackPageview']];
 		(function(d,t){var g=d.createElement(t),s=d.getElementsByTagName(t)[0];g.async=1;
 		g.src=('https:'==location.protocol?'//ssl':'//www')+'.google-analytics.com/ga.js';
 		s.parentNode.insertBefore(g,s)}(document,'script'));
